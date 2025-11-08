5a8804994e729e3fb9a24b3727b4f78545613374
Gunasagar-Akkabathula <gunaguna1747@gmail.com>
2025-11-08 14:02:27 +0530

    backn to normal

---
+++ categories.sql
@@ -0,0 +1,17 @@
+INSERT INTO `categories` VALUES (1,'Cricket','Cricket gear and equipment'),
+(2,'Volleyball','Volleyballs, nets, and accessories'),
+(3,'Football','Football and soccer products'),
+(4,'Basketball','Basketball gear and accessories'),
+(5,'Baseball','Baseball equipment and gear'),
+(6,'Hockey','Hockey sticks, pads, and accessories'),
+(7,'Tennis','Tennis rackets, balls, and accessories'),
+(8,'Badminton','Badminton rackets and shuttlecocks'),
+(9,'Boxing','Boxing gloves and protective gear'),
+(10,'Martial Arts','Martial arts equipment and clothing'),
+(11,'Running & Jogging','Running shoes, apparel, and accessories'),
+(12,'Sports Shoes','Various sports footwear'),
+(13,'Sports Apparel','Jerseys, shorts, and other apparel'),
+(14,'Protective Gear','Helmets, pads, and guards'),
+(15,'Sports Bags & Backpacks','Bags for sports equipment and personal use');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
